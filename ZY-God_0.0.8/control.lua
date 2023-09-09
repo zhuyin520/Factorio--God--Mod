1de18e003213af46ac2a05b80f81069a03e94966
朱银 <114227657+zhuyin520@users.noreply.github.com>
2023-09-10 06:35:00 +0800

    Add files via upload

---
+++ ZY-God_0.0.8/control.lua
@@ -0,0 +1,7 @@
+-- control.lua控制游戏逻辑的代码，例如事件处理、物体生成和游戏规则。
+
+require("cosnm_nts")  -- 引入自定义事件模块（鼠标中键触发=启动或禁用MODS）
+require("custom_events")  -- 引入自定义事件模块（鼠标左键=狂雷）
+require("custom_exjmuyhi")  -- 引入自定义事件模块（鼠标右键=瞬移）
+
+
