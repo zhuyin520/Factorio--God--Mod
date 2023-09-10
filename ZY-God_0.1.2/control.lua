e00a2c67703be334e4b488dad1a9f9f2b0e5103e
朱银 <114227657+zhuyin520@users.noreply.github.com>
2023-09-10 21:41:02 +0800

    Add files via upload

---
+++ ZY-God_0.1.2/control.lua
@@ -0,0 +1,66 @@
+-- control.lua控制游戏逻辑的代码，例如事件处理、物体生成和游戏规则。
+
+-- 在控制文件中预先引入所有模块
+local custom_events = require("custom_events")
+local custom_exjmuyhi = require("custom_exjmuyhi")
+
+-- 定义 "mm-Love" 事件处理函数
+function mmLoveEventHandler(event)
+    custom_events.mmLoveEventHandler(event)  -- 使用模块返回的表来访问事件处理函数
+    custom_exjmuyhi.mmLoveEventHandler(event) -- 使用 "custom_exjmuyhi.lua" 模块中的函数或方法
+end
+
+-- 监听 "mm-Love" 事件
+script.on_event("mm-Love", mmLoveEventHandler)
+
+-- 监听 "mm-Love" 事件
+script.on_event("mm-Love-move", mmLoveEventHandler)
+
+-- 鼠标中键=启动或禁用MODS
+
+-- 定义一个全局状态变量，用于控制是否启用 "mm-Love" 事件
+global.mmLoveEnabled = true -- 初始化时默认启用事件
+
+-- 监听 "mm-Love-move-itme" 事件，根据状态变量控制是否启用 "mm-Love" 和 "mm-Love-move" 事件
+script.on_event("mm-Love-move-itme", function(event)
+    local player = game.players[event.player_index]
+    global.mmLoveEnabled = not global.mmLoveEnabled -- 切换状态变量的值
+
+    if global.mmLoveEnabled then
+        player.print("启用，狂雷和传送阵 ")
+        -- 在此处重新绑定 "mm-Love" 事件
+        script.on_event("mm-Love", mmLoveEventHandler)
+        script.on_event("mm-Love-move", mmLoveEventHandler) -- 同时绑定 "mm-Love-move" 事件
+    else
+        player.print("禁用，狂雷和传送阵 ")
+        -- 在此处取消绑定 "mm-Love" 事件
+        script.on_event("mm-Love", nil)
+        script.on_event("mm-Love-move", nil) -- 同时取消绑定 "mm-Love-move" 事件
+    end
+    player.print("mmLoveEnabled 值为：" .. tostring(global.mmLoveEnabled)) -- 输出状态变量的值
+end)
+
+-- 在初始时初始化事件处理
+script.on_init(function()
+    -- 在初始化时，根据状态变量的值绑定或取消绑定 "mm-Love" 和 "mm-Love-move" 事件
+    if global.mmLoveEnabled then
+        script.on_event("mm-Love", mmLoveEventHandler)
+        script.on_event("mm-Love-move", mmLoveEventHandler) -- 同时绑定 "mm-Love-move" 事件
+    else
+        script.on_event("mm-Love", nil)
+        script.on_event("mm-Love-move", nil) -- 同时取消绑定 "mm-Love-move" 事件
+    end
+end)
+
+-- 在MODS状态变化时重新绑定 "mm-Love" 和 "mm-Love-move" 事件
+script.on_configuration_changed(function(data)
+    -- 在 MODS 状态变化时，根据状态变量的值重新绑定或取消绑定 "mm-Love" 和 "mm-Love-move" 事件
+    if global.mmLoveEnabled then
+        script.on_event("mm-Love", mmLoveEventHandler)
+        script.on_event("mm-Love-move", mmLoveEventHandler) -- 同时绑定 "mm-Love-move" 事件
+    else
+        script.on_event("mm-Love", nil)
+        script.on_event("mm-Love-move", nil) -- 同时取消绑定 "mm-Love-move" 事件
+    end
+end)
+
---
+++ ZY-God_0.1.2/potyhsdut/mm_item.lua
@@ -0,0 +1,114 @@
+--mm_item.lua
+
+local mod_path = "__ZY-God__/goigdcht/erfte_ajesty/"
+
+data:extend(
+    {
+        {
+            type = "projectile",
+            name = "MM_chuanshong",
+            flags = { "not-on-map" },   -- 标志表示在地图上不可见
+            acceleration = 0.005,    -- 加速度
+            action = {
+                {
+                    type = "direct",
+                    action_delivery = {
+                        type = "instant",
+                        target_effects = {
+                            {
+                                type = "create-entity",
+                                entity_name = "MM_chuanshong-explosion"
+                            },
+
+                            {
+                                type = "destroy-decoratives",
+                                from_render_layer = "decorative",
+                                to_render_layer = "object",
+                                include_soft_decoratives = true, -- 包括可穿过铁轨路径的软装饰物
+                                include_decals = false,
+                                invoke_decorative_trigger = true,
+                                decoratives_with_trigger_only = false, -- 如果为 true，仅销毁具有 trigger_effect 设置的装饰物
+                                radius = 2.25                    -- 用于演示目的的大半径
+                            }
+                        }
+                    }
+                },
+                {
+                    type = "area",
+                    radius = 0,                 -- 伤害范围半径
+                    action_delivery = {
+                        type = "instant",
+                        target_effects = {
+                            {
+                                type = "damage",
+                                damage = { amount = 0, type = "explosion" }          -- 造成的伤害类型和数值
+                            }
+                        }
+                    }
+                }
+            },
+            light = { intensity = 0.5, size = 4 },     -- 发光属性
+            animation = {
+                filename = "__base__/graphics/entity/grenade/grenade.png",          -- 动画文件路径
+                frame_count = 16,
+                draw_as_glow = true,          -- 作为发光物体渲染
+                frame_count = 16,         -- 帧数
+                line_length = 8,         -- 每行的帧数
+                animation_speed = 0.250,       -- 动画速度
+                width = 26,                       -- 宽度
+                height = 28,                         -- 高度
+                shift = util.by_pixel(1, 1),         -- 位置偏移
+                priority = "high"               -- 优先级
+
+            },
+            shadow = {
+                filename = "__base__/graphics/entity/grenade/grenade-shadow.png",      -- 阴影文件路径
+                frame_count = 16,            -- 帧数    
+                line_length = 8,            -- 每行的帧数
+                animation_speed = 0.250,     -- 动画速度
+                width = 26,                    -- 宽度
+                height = 20,                  -- 高度
+                shift = util.by_pixel(2, 6),           -- 位置偏移
+                priority = "high",           -- 优先级
+                draw_as_shadow = true           -- 作为阴影渲染
+            }
+        },
+        {
+            type = "capsule",               -- 类型为胶囊
+            name = "MM_chuanshong",        
+            localised_name = "传送阵",          -- 本地化名称
+            enabled = false,             -- 设置实体启用状态为true,设置实体禁用状态false            
+            icon = "__ZY-God__/goigdcht/zy/chuanshong.png",
+            icon_size = 64,              --图标大小
+            icon_mipmaps = 1,            --图标个数
+            subgroup = "flower",         -- 子组
+            order = "avfg",          -- 排序顺序
+            capsule_action = {
+                type = "throw",        -- 类型为投掷
+                attack_parameters = {
+                    type = "projectile",
+                    activation_type = "throw",      -- 激活类型
+                    ammo_category = "grenade",      -- 弹药类别
+                    cooldown = 30,         -- 冷却时间
+                    projectile_creation_distance = 0.6,       -- 投掷物创建距离
+                    range = 100,            -- 射程
+                    ammo_type = {
+                        category = "grenade",
+                        target_type = "position",
+                        action = {
+                            {
+                                type = "direct",
+                                action_delivery = {
+                                    type = "projectile",
+                                    projectile = "MM_chuanshong",
+                                    starting_speed = 30           -- 初始速度
+                                }
+                            },
+                        }
+                    }
+                }
+            },
+            stack_size = 100,           --堆叠大小
+        }
+    }
+)
